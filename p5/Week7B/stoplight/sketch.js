b8486339491896b7362dc4eeff405f13ef92216e
chrislbaird <chrislbaird@gmail.com>
2020-02-29 14:19:32 -0600

    dsd
    dsas

--- p5/Week7B/stoplight/sketch.js
+++ p5/Week7B/stoplight/sketch.js
@@ -1,32 +1,35 @@
 var lightsArray = ["Red", "Green", "Yellow"];
 var myState = lightsArray[0];
 var counterForArray = 0;
-var timer = 10;
+var timer = 0;
 var lengthAndWidth = 800;
 
 function setup() {
   createCanvas(lengthAndWidth, lengthAndWidth);
   background(255);
   frameRate(1);
+  background(255);
+  stopLightBackground();
+  redLight(false);
+  yellowLight(false);
+  greenLight(false);
 }
 
 function draw() {
   textSize(16);
-  background(255);
 
+  background(255);
+  stopLightBackground();
   timer--;
   text("myState:  " + myState + "  counterForArray = " + counterForArray + "  Timer: " + timer, 50, 50);
 
-  stopLightBackground();
-  redLight();
-  yellowLight();
-  greenLight();
 
 
 
 
 
-  if (timer == 0) {
+
+  if (timer < 0) {
     myState = lightsArray[counterForArray];
     timer = 10;
     counterForArray = (counterForArray + 1) % lightsArray.length;
@@ -38,17 +41,31 @@ function draw() {
 
     case lightsArray[0]:
     text(lightsArray[0], 100, 100);
-       break ;
+    redLight(true);
+    yellowLight(false);
+    greenLight(false);
+
+
+
+    break;
 
     case lightsArray[1]:
+    text(lightsArray[2], 100, 100);
+    redLight(false);
+    yellowLight(false);
+    greenLight(true);
 
-    text(lightsArray[1], 100, 100);
-       break ;
+
+    break;
 
 
     case lightsArray[2]:
 
-    text(lightsArray[2], 100, 100);
+    text(lightsArray[1], 100, 100);
+    redLight(false);
+    yellowLight(true);
+    greenLight(false);
+
     break ;
 }
 
@@ -59,15 +76,21 @@ function draw() {
 
 function  stopLightBackground() {
    push();
-   fill(255, 204, 0);
+  fill(255, 204, 0);
    rect(100, 100, 100, 300);
    pop();
 
 
 }
 
-function  redLight() {
+function  redLight(flag) {
    push();
+   if (flag) {
+   fill(230, 0, 0);
+   } else {
+   fill(0);
+   }
+   ellipse(150,150,100,100);
 
    pop();
 
@@ -76,20 +99,27 @@ function  redLight() {
 
 
 
-function  yellowLight() {
+function  yellowLight(flag) {
    push();
+   if (flag) {
    fill(231,180,22);
+   } else {
+   fill(0);
+   }
+   ellipse(150,250,100,100);
    pop();
-
-
 }
 
 
 
-function  greenLight() {
+function  greenLight(flag) {
    push();
-   fill(45,201,55);
-
+   if (flag) {
+   fill(76, 187, 23);
+   } else {
+   fill(0);
+   }
+   ellipse(150,350,100,100);
    pop();
 
 
