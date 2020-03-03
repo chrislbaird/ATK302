d3a78e460986b5363c91e9251a5d2b00762dfce0
chrislbaird <chrislbaird@gmail.com>
2020-03-03 10:00:57 -0600

    Update sketch.js

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -17,7 +17,7 @@ function draw() {
 
 
     storyText("'Call me Ishmael. Some years ago--never mind how long precisely--having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.");
-
+    console.log(paragraphBuilder("test"));
 
 
 
