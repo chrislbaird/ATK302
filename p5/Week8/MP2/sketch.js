a4870b868d625ade43fb80dbef3cb2fde9a006e5
chrislbaird <chrislbaird@gmail.com>
2020-03-06 15:03:58 -0600

    Update sketch.js

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -13,13 +13,13 @@ function setup() {
   strokeWeight(4);
   stroke(0);
   fill(0);
-  rect(250,0, 500, 500);
+  rect(250, 0, 500, 500);
   fill(255);
-  rect(750,0, 500, 500);
+  rect(750, 0, 500, 500);
   fill(124);
   rect(250, 500, 1000, 250);
 
-  text("Please Click to Start Story", 300, 550, 800, 500);
+  text("Please Click to Start Story", 301, 550, 800, 500);
 
 }
 
@@ -27,89 +27,89 @@ function draw() {
 
 
   switch (combineStoryTracker) {
-   case 0:
-   storyText("His", true);
-   him(300,100);
-     break;
-   case 2:
-   storyText("His", true);
-   him(400,100);
-     break;
-   case 4:
-   storyText("His", true);
-   him(500,100);
-     break;
-   case 6:
-   storyText("His", true);
-   him(600,100);
-     break;
-   case 8:
-   storyText("His", true);
-   him(700,100);
-     break;
-   case 10:
-   storyText("His", true);
-   him(700,200);
-     break;
-   case 12:
-   storyText("His", true);
-   him(700,300);
-    break;
-   case 14:
-   storyText("His", true);
-   him(700,400);
-    break;
-   case 16:
-   storyText("His", true);
-   him(700,450);
-    break;
-   case 1:
-    storyText("Her", false);
-     her(1200,100);
-       break;
-     case 3:
-     storyText("Her", false);
-     her(1100,100);
-       break;
-     case 5:
-     storyText("Her", false);
-     her(1000,100);
-       break;
-     case 7:
-     storyText("Her", false);
-     her(900,100);
-       break;
-     case 9:
-     storyText("Her", false);
-     her(800,100);
-       break;
-     case 11:
-     storyText("Her", false);
-     her(800,200);
-       break;
-     case 13:
-     storyText("Her", false);
-     her(800,300);
+    case 0:
+      storyText("His", true);
+      him(300, 100);
       break;
-     case 15:
-     storyText("Her", false);
-     her(800,400);
+    case 2:
+      storyText("His", true);
+      him(400, 100);
       break;
-     case 17:
-     storyText("Her", false);
-     her(800,450);
+    case 4:
+      storyText("His", true);
+      him(500, 100);
+      break;
+    case 6:
+      storyText("His", true);
+      him(600, 100);
+      break;
+    case 8:
+      storyText("His", true);
+      him(700, 100);
+      break;
+    case 10:
+      storyText("His", true);
+      him(700, 200);
+      break;
+    case 12:
+      storyText("His", true);
+      him(700, 300);
+      break;
+    case 14:
+      storyText("His", true);
+      him(700, 400);
+      break;
+    case 16:
+      storyText("His", true);
+      him(700, 450);
+      break;
+    case 1:
+      storyText("Her", false);
+      her(1200, 100);
+      break;
+    case 3:
+      storyText("Her", false);
+      her(1100, 100);
+      break;
+    case 5:
+      storyText("Her", false);
+      her(1000, 100);
+      break;
+    case 7:
+      storyText("Her", false);
+      her(900, 100);
+      break;
+    case 9:
+      storyText("Her", false);
+      her(800, 100);
+      break;
+    case 11:
+      storyText("Her", false);
+      her(800, 200);
+      break;
+    case 13:
+      storyText("Her", false);
+      her(800, 300);
+      break;
+    case 15:
+      storyText("Her", false);
+      her(800, 400);
+      break;
+    case 17:
+      storyText("Her", false);
+      her(800, 450);
       break;
 
     case 18:
-    fill(124);
-    rect(250, 500, 1000, 250);
+      fill(124);
+      rect(250, 500, 1000, 250);
 
-    text("End", 300, 550, 800, 500);
-    clearInterval(myVar);
-    storyStateFlag = true;
-    break;
+      text("End", 300, 550, 800, 500);
+      clearInterval(myVar);
+      storyStateFlag = true;
+      break;
 
- }
+  }
 
 
 
@@ -131,16 +131,16 @@ function storyText(inputText, storyFlag) {
 
   pop();
 
-    push();
+  push();
 
-    if (storyFlag) {
+  if (storyFlag) {
     fill(0, 102, 153);
-    } else {
+  } else {
 
-    fill(255,192,203);
-    }
-    text(inputText, 300, 550, 800, 500);
-    pop();
+    fill(255, 192, 203);
+  }
+  text(inputText, 300, 550, 800, 500);
+  pop();
 
 
 }
@@ -152,13 +152,13 @@ function storyText(inputText, storyFlag) {
 
 function him(x, y) {
 
-    push();
-    fill(0);
-    rect(250,0, 500, 500);
+  push();
+  fill(0);
+  rect(250, 0, 500, 500);
 
-    fill(255);
-    circle(x, y, 100);
-    pop();
+  fill(255);
+  circle(x, y, 100);
+  pop();
 
 }
 
@@ -168,7 +168,7 @@ function her(x, y) {
 
   push();
   fill(255);
-  rect(750,0, 500, 500);
+  rect(750, 0, 500, 500);
   fill(0);
   circle(x, y, 100);
   pop();
@@ -178,10 +178,10 @@ function her(x, y) {
 
 
 function mousePressed() {
- if (storyStateFlag) {
-  myVar = setInterval(moveStory, 5000);
-  storyStateFlag = false;
- }
+  if (storyStateFlag) {
+    myVar = setInterval(moveStory, 5000);
+    storyStateFlag = false;
+  }
 
 
 }
@@ -212,6 +212,6 @@ function herStoryFunction() {
 
 
 function moveStory() {
-   console.log(combineStoryTracker);
-   combineStoryTracker++;
+  console.log(combineStoryTracker);
+  combineStoryTracker++;
 }
