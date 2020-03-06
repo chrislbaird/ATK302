3dec1b5530179d17b889a4d754494a70571308c0
chrislbaird <chrislbaird@gmail.com>
2020-03-06 13:48:22 -0600

    das
    sdadsa

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -65,39 +65,39 @@ function draw() {
     break;
    case 1:
     storyText("Her", false);
-     her(1200,100);
+     her(1150,100);
        break;
      case 3:
      storyText("Her", false);
-     her(1100,100);
+     her(1050,100);
        break;
      case 5:
      storyText("Her", false);
-     her(1000,100);
+     her(950,100);
        break;
      case 7:
      storyText("Her", false);
-     her(900,100);
+     her(850,100);
        break;
      case 9:
      storyText("Her", false);
-     her(800,100);
+     her(750,100);
        break;
      case 11:
      storyText("Her", false);
-     her(800,200);
+     her(750,200);
        break;
      case 13:
      storyText("Her", false);
-     her(800,300);
+     her(750,300);
       break;
      case 15:
      storyText("Her", false);
-     her(800,400);
+     her(750,400);
       break;
      case 17:
      storyText("Her", false);
-     her(800,450);
+     her(750,450);
        break;
 
     case 18:
