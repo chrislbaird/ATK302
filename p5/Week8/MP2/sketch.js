76884ee0524a630a6bc4840cbd1a19bdde52c119
chrislbaird <chrislbaird@gmail.com>
2020-03-06 18:08:14 -0600

    Update sketch.js

--- p5/Week8/MP2/sketch.js
+++ p5/Week8/MP2/sketch.js
@@ -28,27 +28,27 @@ function draw() {
 
   switch (combineStoryTracker) {
     case 0:
-      storyText("I was 25 years old just starting my first job after college. I had just moved to a new city and did not know anyone.", true);
+      storyText("", true);
       him(300, 100);
       break;
     case 1:
-      storyText("I was 33 and starting a new point in my life.", false);
+      storyText("", false);
       her(1200, 100);
       break;
     case 2:
-      storyText("I spent a lot of free time walking around the city just so I could see someone. I did not have the courage to interact with anyone.", true);
+      storyText("", true);
       him(400, 100);
       break;
     case 3:
-      storyText("I had just finalized my divorce.  I had no desire to see or to talk to anyone.", false);
+      storyText("", false);
       her(1100, 100);
       break;
     case 4:
-      storyText("My favorite place to walk was a park near my house.", true);
+      storyText("", true);
       him(500, 100);
       break;
     case 5:
-      storyText("I spent most of my days off of work alone. I could not stop thinking about the friends, family and of the marriage that I lost. It was a normal day in the park when I saw her sitting on a bench. She was playing with something in her lap.", false);
+      storyText("", false);
       her(1000, 100);
       break;
     case 6:
@@ -56,7 +56,7 @@ function draw() {
       him(600, 100);
       break;
     case 7:
-      storyText("I would sit on the bench for hours just waiting for time  to pass so I could go home to my empty apartment.", false);
+      storyText(".", false);
       her(900, 100);
       break;
     case 8:
@@ -64,7 +64,7 @@ function draw() {
       him(700, 100);
       break;
     case 9:
-      storyText("One day, while sitting on the bence I felt something brush against my leg.", false);
+      storyText("", false);
       her(800, 100);
       break;
     case 10:
@@ -72,31 +72,31 @@ function draw() {
       him(700, 200);
       break;
     case 11:
-      storyText("It was a tiny kitten trying to play with my shoestrings.", false);
+      storyText("", false);
       her(800, 200);
       break;
     case 12:
-      storyText("She looked like she was in her own world nothing around her seemed to matter.", true);
+      storyText("", true);
       him(700, 300);
       break;
     case 13:
-      storyText("I picked her up and placed her in my lap.", false);
+      storyText("", false);
       her(800, 300);
       break;
     case 14:
-      storyText("In a blink of an eye the kitten made a dart towards the other end of the park ", true);
+      storyText("", true);
       him(700, 400);
       break;
     case 15:
-      storyText("While playing with the kitten,  I noticed him again. I tried to make it seem that I was only focused on the kitten", false);
+      storyText("", false);
       her(800, 400);
       break;
     case 16:
-      storyText("I picked up the kitten and before I knew it, she was standing right next to me.", true);
+      storyText("", true);
       him(700, 450);
       break;
     case 17:
-      storyText("A car honking in the distance caused the kitten to get scared and run away. ", false);
+      storyText(" ", false);
       her(800, 450);
       break;
     case 18:
