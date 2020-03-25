6357376ac4dae32cf01581ed2c803f5f0715ac75
chrislbaird <chrislbaird@gmail.com>
2020-03-25 06:59:33 -0500

    Update sketch.js
    adsfvasdff

--- p5/Week11/OOP1/sketch.js
+++ p5/Week11/OOP1/sketch.js
@@ -4,8 +4,8 @@ var testSquare;
 function setup() {
   createCanvas(1000, 1000);
   testSquare = new squares(100, 100, "red", 1000, 1000, 50, 50, 1, -1);
-  testSquareBeta = new squares(800, 800, "green", 900, 900, 50, 50, 1, 1);
-  testSquareAlpha = new squares(400, 400, "blue", 500, 500, 50, 50, -1, -1);
+  testSquareBeta = new squares(800, 800, "green", 1000, 1000, 50, 50, 1, 1);
+  testSquareAlpha = new squares(400, 400, "blue", 1000, 1000, 50, 50, -1, -1);
   background("Black");
 }
 
