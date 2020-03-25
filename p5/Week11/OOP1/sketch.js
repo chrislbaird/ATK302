60b28f925e392beec0147fabb5a916c0db1d8e1c
chrislbaird <chrislbaird@gmail.com>
2020-03-25 07:10:39 -0500

    dfsdf
    sfsd

--- p5/Week11/OOP1/sketch.js
+++ p5/Week11/OOP1/sketch.js
@@ -35,15 +35,21 @@ function squares(xLocation, yLocation, squareColour, yBounds, xBounds, XSize, YS
   this.display = function()
   {
     
-  if (this.xLocation + 50 > this.xBounds ||   this.xLocation - 50 < 0) {
+  if (this.xLocation + 50 > this.xBounds ||   this.xLocation  < 0) {
     this.xDirection = this.xDirection * -1;
   }
 
-  if (this.yLocation + 50 > this.yBounds ||   this.yLocation - 50 < 0) {
+  if (this.yLocation + 50 > this.yBounds ||   this.yLocation  < 0) {
     this.yDirection = this.yDirection * -1;
   }
 
+  if (this.xLocation + 50 > this.xBounds ||   this.xLocation < 0) {
+    this.xDirection = this.xDirection * -1;
+  }
 
+  if (this.yLocation + 50 > this.yBounds ||   this.yLocation  < 0) {
+    this.yDirection = this.yDirection * -1;
+  }
 
 
 
