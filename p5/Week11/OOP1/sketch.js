a62af591d0fb2221f9431be8911336a128ff97f2
chrislbaird <chrislbaird@gmail.com>
2020-03-25 06:03:39 -0500

    ted

--- p5/Week11/OOP1/sketch.js
+++ p5/Week11/OOP1/sketch.js
@@ -1,7 +1,36 @@
+var testSquare;
+
 function setup() {
-  // put setup code here
+  createCanvas(1000, 1000);
+  testSquare = new squares(100, 100, "red", 1000, 1000, 100, 100);
 }
 
 function draw() {
-  // put drawing code here
+  testSquare.display();
+}
+
+
+
+function squares(xLocation, yLocation, squareColour, yBounds, xBounds, XSize, YSize) {
+
+  //attributes
+  this.xLocation = xLocation;
+  this.yLocation = yLocation;
+  this.squareColour = squareColour;
+  this.yBounds = yBounds;
+  this.xBounds = xBounds;
+  this.XSize = XSize;
+  this.YSize = YSize;
+  //methods
+  this.display = function()
+  {
+    push();
+    fill(squareColour);
+    rect(this.xLocation, this.yLocation,this.XSize,this.YSize)
+    pop();
+
+
+
+
+  }
 }
