5cb2174b778e44b13c2ca573ff4ec6cc5f48a1f4
chrislbaird <chrislbaird@gmail.com>
2020-03-26 02:54:27 -0500

    Update sketch.js

--- p5/Week11/test/sketch.js
+++ p5/Week11/test/sketch.js
@@ -78,12 +78,14 @@ function squares(xLocation, yLocation, squareColour, yBounds, xBounds, XSize, YS
 
   this.boundaryCheck = function name() {
 
-    if (this.xLocation + this.xDirection > this.xBounds || this.xLocation + this.xDirection <= 0) {
-      this.xDirection = this.xDirection * -1;
+    if (this.xLocation > this.xBounds || this.xLocation < 0) {
+      this.xDirection = this.xDirection * - 1;
+
     }
 
-    if (this.yLocation + this.yDirection > this.yBounds || this.yLocation + this.yDirection <= 0) {
-      this.yDirection = this.yDirection * -1;
+    if (this.yLocation > this.yBounds || this.yLocation < 0) {
+      this.yDirection = this.yDirection * - 1;
+
     }
 
   }
