9fe15cb5680d5f420354778c92fde49d067f43ba
chrislbaird <chrislbaird@gmail.com>
2020-03-25 19:15:29 -0500

    argadsdfg
    aerga

--- p5/Week11/test/sketch.js
+++ p5/Week11/test/sketch.js
@@ -118,13 +118,14 @@ function squares(xLocation, yLocation, squareColour, yBounds, xBounds, XSize, YS
 
     print("True false statement check:  " + get(mouseX, mouseY) == color("black"));
     colorArray = get(mouseX, mouseY);
-    print("Print Color: " + colorArray[1])
-    if (color(get(mouseX, mouseY)) == color("black")) {
+    print("Print Color Red: " + colorArray[0])
+    print("Print Color Green: " + colorArray[1])
+    print("Print Color Blue: " + colorArray[2])
+    if (colorArray[0] == 0 && colorArray[1] == 0 && colorArray[2] == 0) {
       print("MouseX:   " + mouseX + "   MouseY:  " + mouseY + "   is black")
       print(get(mouseX, mouseY));
 
     }
-
 
 
 
