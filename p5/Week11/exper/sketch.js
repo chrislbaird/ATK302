6733fca51e7654a911183ad78b29592114d866ac
chrislbaird <chrislbaird@gmail.com>
2020-03-26 07:19:54 -0500

    Update sketch.js

--- p5/Week11/exper/sketch.js
+++ p5/Week11/exper/sketch.js
@@ -4,6 +4,19 @@ function setup() {
 }
 
 function draw() {
+  push()
   fill("red");
   rect(500, 500, 100, 100);
+  pop();
+
+
+  push()
+  fill("green");
+  rect(600, 600, 100, 100);
+  pop();
+
+  colorArray = print(600, 600);
+  print("Print Color Red: " + colorArray[0])
+  print("Print Color Green: " + colorArray[1])
+  print("Print Color Blue: " + colorArray[2])
 }
