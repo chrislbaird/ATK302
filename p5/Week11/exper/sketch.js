063aed9caee109e7d4633f1a5bb8c78d00d6fd45
chrislbaird <chrislbaird@gmail.com>
2020-03-29 10:11:53 -0500

    Update sketch.js

--- p5/Week11/exper/sketch.js
+++ p5/Week11/exper/sketch.js
@@ -7,7 +7,7 @@ function draw() {
   for (let i = 0; i < width; i++) {
     for (let j = 0; j < height; j++) {
       let c = color(204 - j, 153 - i, 0);
-      set(i, j, rgb(255, 0, 0));
+      set(i, j, color(255, 0, 0));
       updatePixels();
     }
   }
