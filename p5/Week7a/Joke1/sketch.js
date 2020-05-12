66899421b00bb4b912f52fff338d29c4ccaec582
chrislbaird <chrislbaird@gmail.com>
2020-05-11 20:07:07 -0500

    ree
    er5tr

--- p5/Week7a/Joke1/sketch.js
+++ p5/Week7a/Joke1/sketch.js
@@ -1,7 +1,41 @@
+var myState = 0
+
+
 function setup() {
-  // put setup code here
+  createCanvas(500, 500);
 }
 
 function draw() {
-  // put drawing code here
+
+  switch (myState) {
+
+    case 0:
+      background(128);
+      text("What is the best thing to do if your program does not run?", 100, 100);
+
+      break;
+
+    case 1:
+      background(128);
+
+      text("Change Nothing in the code and run the program again.", 100, 100);
+
+      break;
+
+
+
+  }
+
+
+
+
+
+}
+
+function mouseClicked() {
+  myState = (myState + 1) % 2;
+
+  console.log(myState);
+
+
 }
--- p5/Week7a/joke2/sketch.js
+++ p5/Week7a/joke2/sketch.js
@@ -1,7 +1,45 @@
+var myState = 0
+var secounds = 0;
+var myVar;
+
 function setup() {
-  // put setup code here
+  createCanvas(500, 500);
+  myVar = window.setInterval(timerFunction, 5000);
+
 }
 
 function draw() {
-  // put drawing code here
+  text(secounds, 10, 10);
+  switch (myState) {
+
+    case 0:
+      background(128);
+      text("What is the differance between an outlow and a inlaw?", 100, 100);
+
+      break;
+
+    case 1:
+      background(128);
+
+      text("A outlaw is wanted", 100, 100);
+
+      break;
+
+
+
+  }
+
+
+
+
+
+}
+
+function timerFunction() {
+  myState = (myState + 1) % 2;
+
+  console.log(myState);
+
+  secounds = secounds + 1;
+
 }
