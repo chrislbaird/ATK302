2e47c00547a19ed0ce52e973674a3d7fc3d1997a
chrislbaird <chrislbaird@gmail.com>
2020-05-11 20:25:42 -0500

    safgadfs
    saddf

--- p5/Week7a/multSound/sketch.js
+++ p5/Week7a/multSound/sketch.js
@@ -1,7 +1,52 @@
+// Starter audio code (“Sounds” from Week 4) put in sketch.js
+var myState = 0
+var song1;
+
+function preload() {
+  song1 = loadSound('assets/Zanarkand.mp3');
+}
+
 function setup() {
-  // put setup code here
+
+  createCanvas(720, 200);
+  background(255, 0, 0);
+  song1.play(); // this is what starts the sound
+
 }
 
 function draw() {
-  // put drawing code here
+
+  switch (myState) {
+    case 0:
+      // code block
+      break;
+    case 1:
+      // code block
+      break;
+    case 2:
+      // code block
+  }
+
+
+
+
+
+
+
+
+}
+
+function mouseReleased() {
+  if (song1.isPlaying()) {
+    song1.pause();
+  } else {
+    song1.play();
+  }
+
+
+}
+
+
+function touchStarted() {
+  getAudioContext().resume();
 }
